01829e8113418a4efa30635585bdd11a5f0a545b
Рустам <rusiq@list.ru>
2017-05-11 02:31:09 +1000

    remove bug when store plugin setting

--- engine/includes/inc/extrainst.inc.php
+++ engine/includes/inc/extrainst.inc.php
@@ -69,7 +69,7 @@ function generate_config_page($module, $params, $values = array()) {
 		} elseif ($param['type'] == 'select') {
 			$tvars['input'] = '<select name="'.$param['name'].'" '.$param['html_flags'].' class="form-control">';
 			foreach ($param['values'] as $oid => $oval) {
-				$tvars['input'] .= '<option value="'.secure_html($param['value']).'"'.($param['value']==$oid?' selected':'').'>'.$oval.'</option>';
+				$tvars['input'] .= '<option value="'.$oid.'"'.($param['value']==$oid?' selected':'').'>'.$oval.'</option>';
 			}
 			$tvars['input'] .='</select>';
 		} else if ($param['type'] == 'manual') {
@@ -159,6 +159,8 @@ function load_commit_params($cfg, $outparams) {
 
 // Priint page with config change complition notification
 function print_commit_complete($plugin, $cfg) {
+	// Load cofig
+	pluginsLoadConfig();
 	generate_config_page($plugin, $cfg);
 	msg(array('message' => __('commited')));
 }
