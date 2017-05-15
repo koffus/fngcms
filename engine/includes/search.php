b7492e73ff5bb6658de51acc42a629d414362997
Рустам <rusiq@list.ru>
2017-05-15 21:03:58 +1000

    finally add to TWIG - module 'Search'

--- engine/includes/search.php
+++ engine/includes/search.php
@@ -11,22 +11,18 @@
 if (!defined('NGCMS')) die ('HAL');
 
 Lang::load('search', 'site');
-
-//
-// Make search
-//
 include_once root.'includes/news.php';
 
-function search_news(){
-	global $catz, $catmap, $mysql, $config, $userROW, $tpl, $parse, $template, $PFILTERS, $SYSTEM_FLAGS, $TemplateCache;
+function search_news() {
+	global $catz, $catmap, $mysql, $config, $userROW, $template, $twig, $twigLoader, $parse, $PFILTERS, $SYSTEM_FLAGS, $TemplateCache;
 
-	$_REQUEST['author'] = secure_html($_REQUEST['author']);
-	$_REQUEST['catid'] = intval(secure_html($_REQUEST['catid']));
-	$_REQUEST['postdate'] = secure_html($_REQUEST['postdate']);
-	$_REQUEST['search'] = secure_html($_REQUEST['search']);
-	$_REQUEST['orderby'] = secure_html($_REQUEST['orderby']);
-	$_REQUEST['page'] = intval(secure_html($_REQUEST['page']));
-	$submit = isset($_REQUEST['submit']) ? true : false;
+	$author		= isset($_REQUEST['author']) ? secure_html($_REQUEST['author']) : false;
+	$catid		= isset($_REQUEST['catid']) ? intval(secure_html($_REQUEST['catid'])) : false;
+	$postdate	= isset($_REQUEST['postdate']) ? secure_html($_REQUEST['postdate']) : false;
+	$search		= isset($_REQUEST['search']) ? secure_html($_REQUEST['search']) : false;
+	$orderby	= isset($_REQUEST['orderby']) ? secure_html($_REQUEST['orderby']) : false;
+	$page		= isset($_REQUEST['page']) ? intval(secure_html($_REQUEST['page'])) : false;
+	$submit		= isset($_REQUEST['submit']) ? true : false;
 
 	$SYSTEM_FLAGS['info']['title']['group'] = __('search.title');
 
@@ -34,17 +30,17 @@ function search_news(){
 	$filter = array();
 	
 	// AUTHOR
-	if ( $_REQUEST['author'] ) {
-		array_push($filter, array('DATA', 'author', '=', $_REQUEST['author']));
+	if ( $author ) {
+		array_push($filter, array('DATA', 'author', '=', $author));
 	}
 
 	// CATEGORY
-	if ( $_REQUEST['catid'] ) {
-		array_push($filter, array('DATA', 'category', '=', $_REQUEST['catid']));
+	if ( $catid ) {
+		array_push($filter, array('DATA', 'category', '=', $catid));
 	}
 
 	// POST DATE
-	if ( $_REQUEST['postdate'] and preg_match('#^(\d{4})(\d{2})$#', $_REQUEST['postdate'], $dv) ) {
+	if ( $postdate and preg_match('#^(\d{4})(\d{2})$#', $postdate, $dv) ) {
 		if ( ($dv[1] >= 1970)&&($dv[1] <= 2100)&&($dv[2] >=1)&&($dv[2] <= 12) ) {
 			array_push($filter, array('OR',
 				array('DATA', 'postdate', 'BETWEEN', array(mktime(0,0,0,$dv[2],1,$dv[1]), mktime(23,59,59,$dv[2],date("t",mktime(0,0,0,$dv[2],1,$dv[1])),$dv[1]))),
@@ -53,32 +49,31 @@ function search_news(){
 	}
 
 	// TEXT
-	$search = array();
-	if ( $_REQUEST['search'] ) {
-		$search_words = trim(str_replace(array('<', '>', '%', '$', '#'), '', mb_substr($_REQUEST['search'], 0, 64, 'UTF-8')));
+	$search_array = array();
+	if ( $search ) {
+		$search_words = trim(str_replace(array('<', '>', '%', '$', '#'), '', mb_substr($search, 0, 64, 'UTF-8')));
 		$search_words = preg_split('/[ \,\.]+/', $search_words, -1, PREG_SPLIT_NO_EMPTY);
 
 		foreach ($search_words as $s) {
 			if ( mb_strlen($s, 'UTF-8') > 2 ) {
-				array_push($search,
+				array_push($search_array,
 					array('OR',
 							array('DATA', 'title', 'like', '%'.$mysql->db_quote($s).'%'),
 							array('DATA', 'content', 'like', '%'.$mysql->db_quote($s).'%'),
 					));
-				$search_array[] = $s;
 			} else {
 				unset($s);
 			}
 		}
 
-		if ( count($search) > 1 ) {
-			array_unshift($search, 'AND');
+		if ( count($search_array) > 1 ) {
+			array_unshift($search_array, 'AND');
 		}
-		if ( count($search) == 1 ) {
-			$search = $search[0];
+		if ( count($search_array) == 1 ) {
+			$search_array = $search_array[0];
 		}
-		if ( count($search) > 0 ) {
-			array_push($filter, $search);
+		if ( count($search_array) > 0 ) {
+			array_push($filter, $search_array);
 		}
 	}
 
@@ -94,19 +89,29 @@ function search_news(){
 	loadActionHandlers('news:search');
 
 	// Configure pagination
-	$paginationParams = array('pluginName' => 'search', 'xparams' => array('search' => $_REQUEST['search'], 'author' => $_REQUEST['author'], 'catid' => $_REQUEST['catid'], 'postdate' => $_REQUEST['postdate'], 'newsOrder' =>$_REQUEST['orderby']), 'paginator' => array('page', 1, false));
-
-	// Configure News order
-	$orderlist = '<option value="">'.__('search.order_default').'</option>';
-	foreach (array('id desc', 'id asc', 'postdate desc', 'postdate asc', 'title desc', 'title asc') as $v) {
-		$vx = str_replace(' ','_',$v);
-		$orderlist.='<option value="'.$v.'"'.((secure_html($_REQUEST['orderby'])==$v)?' selected="selected"':'').'>'.__('search.order_'.$vx).'</option>';
-	}
+	$paginationParams = array(
+		'pluginName' => 'search',
+		'xparams' => array(
+			'search' => $search,
+			'author' => $author,
+			'catid' => $catid,
+			'postdate' => $postdate,
+			'newsOrder' => $orderby,
+			),
+		'paginator' => array('page', 1, false));
 
 	// Configure display params
-	$callingParams = array('style' => 'short','searchFlag' => true, 'extendedReturn' => true, 'entendedReturnPagination' => true, 'customCategoryTemplate' => false, 'newsOrder' =>$_REQUEST['orderby']);
-	if ( $_REQUEST['page'] ) {
-		$callingParams['page'] = $_REQUEST['page'];
+	$callingParams = array(
+		'style' => 'short',
+		'searchFlag' => true,
+		'extendedReturn' => true,
+		'extendedReturnData' => true,
+		'entendedReturnPagination' => true,
+		'customCategoryTemplate' => false,
+		'newsOrder' => $orderby,
+		);
+	if ( $page ) {
+		$callingParams['page'] = $page;
 	}
 
 	// Preload template configuration variables
@@ -118,45 +123,45 @@ function search_news(){
 	}
 
 	// Call SEARCH only if search words are entered
+	$tableVars = array();
 	if ( count($search_array) ) {
-		$found = news_showlist($filter, $paginationParams, $callingParams);
+		$tableVars = news_showlist($filter, $paginationParams, $callingParams);
 	} else {
-		$found = array('count' => 0, 'data' => false);
+		$tableVars = array('count' => 0, 'data' => false);
 	}
 
 	// Now let's show SEARCH basic template
-	$tpl -> template('search.table', tpl_dir.$config['theme']);
-	$tvars = array();
-	$tvars['vars']['author'] = secure_html($_REQUEST['author']);
-	$tvars['vars']['search'] = secure_html($_REQUEST['search']);
-	$tvars['vars']['orderlist'] = $orderlist;
-	$tvars['vars']['searchSettings'] = (isset($_COOKIE['searchSettings']) and $_COOKIE['searchSettings']) ? '' : 'collapsed';
-	$tvars['vars']['viewSettings'] = (isset($_COOKIE['searchSettings']) and $_COOKIE['searchSettings']) ? ' in' : '';
-	$tvars['vars']['checkSettings'] = (isset($_COOKIE['searchSettings']) and $_COOKIE['searchSettings']) ? '  checked="checked"' : '';
-
-	$tvars['vars']['count'] = $found['count'];
-	$tvars['vars']['form_url'] = generateLink('search', '', array());
-
-	$tvars['regx']['#\[found\](.+?)\[/found\]#is'] = (isset($_REQUEST['search']) and count($search_array) and $found['count'])?'$1':'';
-	$tvars['regx']['#\[notfound\](.+?)\[/notfound\]#is'] = (isset($_REQUEST['search']) and count($search_array) and !$found['count'])?'$1':'';
-	$tvars['regx']['#\[error\](.+?)\[/error\]#is'] = ($submit and isset($_REQUEST['search']) and !count($search_array))?'$1':'';
+	$tableVars['form_url'] = generateLink('search', '', array());
+	$tableVars['author'] = $author;
+	$tableVars['search'] = $search;
+	$tableVars['searchSettings'] = (isset($_COOKIE['searchSettings']) and $_COOKIE['searchSettings']) ? '  checked="checked"' : '';
+	$tableVars['flags'] = array(
+		'found'			=> (count($search_array) and $tableVars['count']) ? 1 : 0,
+		'notfound'	=> (count($search_array) and !$tableVars['count']) ? 1 : 0,
+		'error'	=> ( ($submit and $search and !count($search_array)) or ($submit and empty($search)) ) ? 1 : 0,
+		);
 
 	// Make category list
-	$tvars['vars']['catlist'] = makeCategoryList( array ('name' => 'catid', 'selected' => $_REQUEST['catid'], 'doempty' => 1));
+	$tableVars['catlist'] = makeCategoryList( array ('name' => 'catid', 'selected' => $catid, 'doempty' => 1));
 
-	// Results of search
-	$tvars['vars']['entries'] = $found['data'];
-	$tvars['vars']['pagination'] = $found['pagination'];
 	// Make month list
 	$mnth_list = explode(',', __('months'));
 	foreach ( $mysql->select("SELECT month(from_unixtime(postdate)) as month, year(from_unixtime(postdate)) as year, COUNT(id) AS cnt FROM " . prefix . "_news WHERE approve = '1' GROUP BY year, month ORDER BY year DESC, month DESC") as $row ) {
-
 		$pd_value = sprintf("%04u%02u",$row['year'],$row['month']);
 		$pd_text = $mnth_list[$row['month']-1].' '.$row['year'];
-
-		$tvars['vars']['datelist'] .= "<option value=\"".$pd_value."\"".(($pd_value == $_REQUEST['postdate'])?' selected':'').">".$pd_text."</option>";
+		$tableVars['datelist'] .= '<option value="'.$pd_value.'"'.(($pd_value == $postdate)?' selected':'').'>'.$pd_text.'</option>';
 	}
 
-	$tpl -> vars('search.table', $tvars);
-	$template['vars']['mainblock'] .= $tpl -> show('search.table');
+	// Make News order list
+	$tableVars['orderlist'] = '<option value="">'.__('search.order_default').'</option>';
+	foreach (array('id desc', 'id asc', 'postdate desc', 'postdate asc', 'title desc', 'title asc') as $v) {
+		$vx = str_replace(' ','_',$v);
+		$tableVars['orderlist'] .= '<option value="'.$v.'"'.($orderby == $v ? ' selected="selected"' : '').'>'.__('search.order_'.$vx).'</option>';
+	}
+
+	// Results of search
+	$sTemplateName =  tpl_dir.$config['theme'] . '/search.table.tpl';
+	$twigLoader->setDefaultContent($sTemplateName, '{% for entry in data %}{{ entry }}{% else %}' . __('search.notfound') .'{% endfor %} {{ pagination }}');
+	$xt = $twig->loadTemplate($sTemplateName);
+	$template['vars']['mainblock'] .= $xt->render($tableVars);
 }
